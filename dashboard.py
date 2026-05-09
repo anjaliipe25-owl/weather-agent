0485db0f209158693e428c1122e4b4704772c046
anjaliipe25-owl <anjaliipe25@gmail.com>
2026-05-09 12:57:10 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,3 +2,9 @@ import streamlit as st
 import plotly.graph_objects as go
 import json
 import pandas as pd
+
+with open("agent_state.json", "r") as f:
+    history = json.load(f)
+
+df = pd.DataFrame(history)
+print(df)
